9743357ad1b49057612dad48355ed93cc108b12a
Troy Scott <troys2005@gmail.com>
2012-04-27 22:08:36 -0700

    static files

--- webdev/settings.py
+++ webdev/settings.py
@@ -35,17 +35,24 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/home/dotcloud/data/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
+
+
+# Absolute file path to static files
+STATIC_ROOT = '/home/dotcloud/volatile/static/'
+
+
+STATIC_URL = '/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = ')675@krsxif+k^+d&$v0*wusj_wm)c5$&$_43o(^e0t*m(xr0)'
